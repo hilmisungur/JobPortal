76f12bd3eb1501a7ca306ff1af59cba5e0a5d790
hilmisungur <hilmisungur2022@gmail.com>
2025-05-19 17:21:38 +0300

    güncelleme

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,18 +1,18 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = ""; // Yerel sunucu için varsayılan; canlı sistemde değiştirin
-$dbname = "job_portal";
+include "db_config.php"; // Kendi bağlantı ayarlarını buradan alır
 
+// Port tanımlıysa 5 parametreli, değilse 4 parametreli bağlantı kullan
+if (isset($port)) {
+    $conn = new mysqli($servername, $username, $password, $dbname, $port);
+} else {
+    $conn = new mysqli($servername, $username, $password, $dbname);
+}
 
-// Bağlantıyı oluştur
-$conn = new mysqli($servername, $username, $password, $dbname   );
 $conn->set_charset("utf8mb4");
 
 // Hata kontrolü
 if ($conn->connect_error) {
     error_log("Database connection failed: " . $conn->connect_error);
-    // Kullanıcıya hata gösterilmez, yönlendirme yapılabilir
     die("We are currently experiencing technical difficulties. Please try again later.");
 }
 ?>
